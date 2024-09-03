e2cc3f647651c11c6c9adfc43d5c40fb528349b1
Endy Muhardin <endy.muhardin@gmail.com>
2024-09-03 11:05:55 +0700

    add coverage test

--- src/test/java/com/sahabatquran/app/web/SahabatquranWebApplicationTests.java
+++ src/test/java/com/sahabatquran/app/web/SahabatquranWebApplicationTests.java
@@ -2,10 +2,11 @@ package com.sahabatquran.app.web;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.UseMainMethod;
 import org.springframework.context.annotation.Import;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(useMainMethod = UseMainMethod.WHEN_AVAILABLE)
 class SahabatquranWebApplicationTests {
 
 	@Test
